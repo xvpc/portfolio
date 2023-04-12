8c6ed2358758788cb5a21a827eae5ae371fa9cd2
Viper <viperxx100@gmail.com>
2023-04-12 16:56:53 +0300

    #

--- pages/index.js
+++ pages/index.js
@@ -85,10 +85,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         {/* ICONS */}
-        <link rel="shortcut icon" type="image/x-icon" href='/favicon/favicon.ico' />
-        <link rel="apple-touch-icon" sizes="180x180" href='/favicon/apple-touch-icon.png' />
-        <link rel="icon" type="image/png" sizes="32x32" href='/favicon/favicon-32x32.png'/>
-        <link rel="icon" type="image/png" sizes="16x16" href='/favicon/favicon-16x16.png'/>
+        <link rel="shortcut icon" type="image/x-icon" href='../public/favicon/favicon.ico' />
+        <link rel="apple-touch-icon" sizes="180x180" href='../public/favicon/apple-touch-icon.png' />
+        <link rel="icon" type="image/png" sizes="32x32" href='../public/favicon/favicon-32x32.png'/>
+        <link rel="icon" type="image/png" sizes="16x16" href='../public/favicon/favicon-16x16.png'/>
       </Head>
       
       <div className={`text-white bg-dark main-content d-flex flex-column justify-content-between min-vh-100`}>
