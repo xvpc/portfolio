dff1ab1b71dfa7663bdc811983520619837e250b
Viper <viperxx100@gmail.com>
2023-09-19 20:26:05 +0300

    $

--- pages/api/projects/create.ts
+++ pages/api/projects/create.ts
@@ -1,5 +1,3 @@
-import type { NextRequest } from 'next/server';
-
 // Cloudflare
 import { D1Database } from "@cloudflare/workers-types";
 
@@ -17,22 +15,24 @@ export default async function handler(request: Request){
     const image = decodeURIComponent(url?.searchParams.get("image") || "") || null;
     const description = decodeURIComponent(url?.searchParams.get("description") || "") || null;
 
-    // // const apiAuth = request.headers?.authorization
-    // // if(apiAuth !== process.env.NEXT_PUBLIC_LOCAL_API_KEY){
-    // //     return Response.json({error: 'Unauthorized User.. Banned Will Be A Respones'})
-    // // }
 
     if(request.method === 'POST'){
         if(!name && !link && !repo && !image && !description){
-            return new Response(JSON.stringify({ error: '/api/projects/create?name=projectname&link=projectlink&repo=projectrepo&description=projectdescription'}))
+            return new Response(JSON.stringify({ error: '/api/projects/create?name=projectname&link=projectlink&repo=projectrepo&image=Projectimage&description=projectdescription'}))
         }
     
+        const apiKey = request.headers?.get("authorization") || ""
+    
+        if(apiKey !== process.env.NEXT_PUBLIC_API_KEY){
+            return new Response(JSON.stringify({ error: 'Unauthorized User'}))
+        }
+
         // @ts-ignore
         const { DB } = (process.env as { DB: D1Database }); 
     
         try{
-            const { results } = await DB.prepare('INSERT INTO Projects (name, link, image, repo, description) VALUES (?1,?2 ,?3 ,?4 ,?5 );').bind(name, link || "", image || "", image || "", description || "").run()
-            return new Response(JSON.stringify({ results: results ?? null }))
+            const { success } = await DB.prepare('INSERT INTO Projects (name, link, repo, image, description) VALUES (?1,?2 ,?3 ,?4 ,?5 );').bind(name, link || "", repo || "", image || "", description || "").run()
+            return new Response(JSON.stringify({ success: success }))
         }catch(err: any){
             return new Response(JSON.stringify({ error: err.message ?? 'Something went wrong'}))
         }
