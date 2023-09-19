567168ca715e3449882c87f47a61f614883b674b
Viper <viperxx100@gmail.com>
2023-09-19 20:07:30 +0300

    #

--- pages/api/projects/create.ts
+++ pages/api/projects/create.ts
@@ -10,30 +10,29 @@ export const config = {
 
 export default async function handler(request: Request){
     const url = new URL(request.url);
-    return new Response(JSON.stringify({ error: url ?? 'Something went wrong'}))
 
-    // const name = decodeURIComponent(url?.searchParams.get("name") || "");
-    // const link = decodeURIComponent(url?.searchParams.get("link") || "");
-    // const repo = decodeURIComponent(url?.searchParams.get("repo") || "");
-    // const image = decodeURIComponent(url?.searchParams.get("image") || "");
-    // const description = decodeURIComponent(url?.searchParams.get("description") || "");
+    const name = decodeURIComponent(url?.searchParams.get("name") || "");
+    const link = decodeURIComponent(url?.searchParams.get("link") || "");
+    const repo = decodeURIComponent(url?.searchParams.get("repo") || "");
+    const image = decodeURIComponent(url?.searchParams.get("image") || "");
+    const description = decodeURIComponent(url?.searchParams.get("description") || "");
 
     // // const apiAuth = request.headers?.authorization
     // // if(apiAuth !== process.env.NEXT_PUBLIC_LOCAL_API_KEY){
     // //     return Response.json({error: 'Unauthorized User.. Banned Will Be A Respones'})
     // // }
 
-    // if(request.method === 'POST'){
-    //     if(!name || !link || !repo || !image || !description) return Response.json({error: '/api/projects/create?name=projectname&link=projectlink&repo=projectrepo&description=projectdescription'});
+    if(request.method === 'POST'){
+        // if(!name && !link && !repo && !image && !description) return Response.json({error: '/api/projects/create?name=projectname&link=projectlink&repo=projectrepo&description=projectdescription'});
     
-    //     // @ts-ignore
-    //     const { DB } = (process.env as { DB: D1Database }); 
+        // @ts-ignore
+        const { DB } = (process.env as { DB: D1Database }); 
     
-    //     try{
-    //         const results = await DB.prepare('INSERT INTO Projects (name, link, image, repo, description) VALUES (?1,?2 ,?3 ,?4 ,?5 );').bind(name, link || "", image || "", image || "", description || "").run()
-    //         return new Response(JSON.stringify({ results: results ?? null }))
-    //     }catch(err: any){
-    //         return new Response(JSON.stringify({ error: err.message ?? 'Something went wrong'}))
-    //     }
-    // }
+        try{
+            const { results } = await DB.prepare('INSERT INTO Projects (name, link, image, repo, description) VALUES (?1,?2 ,?3 ,?4 ,?5 );').bind(name, link || "", image || "", image || "", description || "").run()
+            return new Response(JSON.stringify({ results: results ?? null }))
+        }catch(err: any){
+            return new Response(JSON.stringify({ error: err.message ?? 'Something went wrong'}))
+        }
+    }
 }
